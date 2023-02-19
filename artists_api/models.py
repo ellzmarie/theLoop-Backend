72851472713e6b8d26a87494191ef81fb710880e
ellzmarie <elliematutisdigal@gmail.com>
2023-02-18 16:34:15 -0800

    updates models

--- artists_api/models.py
+++ artists_api/models.py
@@ -5,6 +5,6 @@ class Artist(models.Model):
   name = models.CharField(max_length=100)
   image = models.CharField(max_length=300)
   link = models.CharField(max_length=1000)
-  description = models.CarField(max_length=1000)
+  description = models.CharField(max_length=1000)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
