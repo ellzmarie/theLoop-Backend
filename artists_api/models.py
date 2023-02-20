ad97fede258e9895e26068019b41516582d8f6bf
ellzmarie <elliematutisdigal@gmail.com>
2023-02-19 17:22:02 -0800

    updates the show page

--- artists_api/models.py
+++ artists_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Artist(models.Model):
   name = models.CharField(max_length=100)
-  image = models.CharField(max_length=300)
+  image = models.CharField(max_length=5000)
   link = models.CharField(max_length=1000)
   description = models.CharField(max_length=1000)
   created_at = models.DateTimeField(auto_now_add=True)
