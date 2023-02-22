fcd841531f66691819ec86989ecb89450ca7c299
ellzmarie <elliematutisdigal@gmail.com>
2023-02-22 13:34:41 -0800

    updates links

--- artists_api/models.py
+++ artists_api/models.py
@@ -9,3 +9,4 @@ class Artist(models.Model):
   category = models.CharField(max_length=100)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
